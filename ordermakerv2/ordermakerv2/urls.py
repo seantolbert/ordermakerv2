4d3ed24b7ead5c06a8bc2ad50727e04c29a1606e
seantolbert <84528717+seantolbert@users.noreply.github.com>
2022-01-11 16:10:37 -0500

    successfully creates oscar template ans stolbert4150@gmail.com superuser

--- ordermakerv2/ordermakerv2/urls.py
+++ ordermakerv2/ordermakerv2/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.apps import apps
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include("myapp.urls")),
+    path("", include(apps.get_app_config("oscar").urls[0])),
+    path("admin/", admin.site.urls),
+    path("pages/", include("django.contrib.flatpages.urls")),
 ]
